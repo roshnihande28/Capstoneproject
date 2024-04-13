c4c46a7eab98f9fba6408fd5bd9853cb375bb73f
roshnihande28 <109075768+roshnihande28@users.noreply.github.com>
2024-04-13 11:49:51 +0530

    Delete django_project directory

--- django_project/project/project/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('django_app.urls')),  # Include URLs from your app
-]
